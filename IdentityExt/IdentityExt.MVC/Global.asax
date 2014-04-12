3561d72012a9ea69ca088b8056ced592e36b7768
ympons <yaismelmp@googlemail.com>
2014-04-12 17:35:50 -0400

    Created project IdentityExt

---
+++ IdentityExt/IdentityExt.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentityExt.MVC.MvcApplication" Language="C#" %>
---
+++ IdentityExt/IdentityExt.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityExt.Web.Startup))]
+namespace IdentityExt.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
