68072cac215fd8d2f897dfe6c09c7ca77673b05e
ympons <yaismelmp@googlemail.com>
2014-04-14 11:41:59 -0400

    Added Group and Role entities

---
+++ IdentityExt/IdentityExt.Web/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentityExt.Web.Models
+{
+    public class Group : IGroup
+    {
+        public Group() { }
+        public Group(string groupName);
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
